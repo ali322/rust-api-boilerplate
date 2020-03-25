5a7acdc788c17c7a73f5a14e62670597b8bcfec1
alichen <ali322@gmail.com>
2020-03-25 11:55:23 +0800

    add APIError

--- src/api/auth.rs
+++ src/api/auth.rs
@@ -1,10 +1,8 @@
-use super::APIResult;
+use crate::api::{APIResult, APIError};
 use crate::dao::{model::*, Conn};
 use diesel::{insert_into, prelude::*};
 use rocket::request::Form;
-use rocket_contrib::json::Json;
-use serde_json::json;
-use std::collections::HashMap;
+use rocket_contrib::json::JsonValue;
 use validator::Validate;
 
 #[derive(FromForm, Validate)]
@@ -19,14 +17,12 @@ pub struct RegisterForm {
 pub fn register(form: Form<RegisterForm>, conn: Conn) -> APIResult {
   use crate::dao::schema::users;
   use chrono::Local;
-  form
-    .validate()
-    .map_err(|e| response!(validate_error!(e), -2))?;
+  form.validate()?;
   let ret = users::table
     .filter(users::username.eq(&form.username))
     .first::<User>(&*conn);
   if ret.is_ok() {
-    return Err(response!("user existed", -2));
+    return Err(APIError::from("use existed!"));
   }
   let now = Local::now().naive_local();
   let user = insert_into(users::table)
@@ -36,8 +32,7 @@ pub fn register(form: Form<RegisterForm>, conn: Conn) -> APIResult {
       users::email.eq(&form.email),
       users::last_logined_at.eq(now),
     ))
-    .get_result::<User>(&*conn)
-    .map_err(|e| response!(e.to_string(), -1))?;
+    .get_result::<User>(&*conn)?;
 
   Ok(response!({
     "token":"123", "user": user
--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,26 +1,53 @@
-use rocket_contrib::json::Json;
-use serde_json::Value;
+use diesel::result::Error as DieselError;
+use rocket::{
+  http::{ContentType, Status},
+  request::Request,
+  response::{Responder, Response, Result as RocketResult},
+};
+use rocket_contrib::json::JsonValue;
+use std::error::Error;
+use std::fmt::{Display, Formatter, self};
+use std::collections::HashMap;
+use validator::{ValidationErrors};
 
-type APIResult = Result<Json<Value>, Json<Value>>;
-
-macro_rules! response {
-  ($val:tt) => {
-    Json(json!({
-      "code": 0,
-      "data" : $val
-    }))
-  };
-  ($val:expr, $code:expr) => {
-    Json(json!({
-      "code": $code,
-      "message" : $val
-    }))
-  };
+#[derive(Debug)]
+pub struct APIError {
+  code: i8,
+  message: JsonValue,
 }
 
-macro_rules! validate_error {
-  ($val:expr) => {
-    $val.field_errors()
+impl Display for APIError{
+  fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+    f.write_str(&format!("{:?}", self))
+  }
+}
+
+impl Error for APIError {}
+
+impl<'r> Responder<'r> for APIError {
+  fn respond_to(self, _: &Request) -> RocketResult<'r> {
+    use std::io::Cursor;
+    let body = json!({"code": self.code, "message": self.message}).to_string();
+    Response::build()
+      .status(Status::Ok)
+      .header(ContentType::new("application", "json"))
+      .sized_body(Cursor::new(body))
+      .ok()
+  }
+}
+
+impl From<DieselError> for APIError {
+  fn from(e: DieselError) -> Self {
+    APIError {
+      code: -2,
+      message: json!(e.to_string()),
+    }
+  }
+}
+
+impl From<ValidationErrors> for APIError{
+  fn from(e: ValidationErrors) -> Self {
+    let errors = e.field_errors()
       .iter()
       .map(|(k, v)| {
         let errors: Vec<String> = v
@@ -34,7 +61,29 @@ macro_rules! validate_error {
           .collect();
         (*k, errors)
       })
-      .collect::<HashMap<_, _>>()
+      .collect::<HashMap<_, _>>();
+      APIError {
+        code: -1,
+        message: json!(errors),
+      }
+  }
+}
+
+impl From<&'static str> for APIError{
+  fn from(e: &'static str) -> Self {
+    APIError{code: -1, message: json!(e)}
+  }
+}
+
+pub type APIResult = Result<JsonValue, APIError>;
+
+
+macro_rules! response {
+  ($val:tt) => {
+    JsonValue::from(json!({
+      "code": 0,
+      "data" : $val
+    }))
   };
 }
 
